ca10c2e781d247b382e394ac451e30634f31c951
Ben Letcher <bletcher@eco.umass.edu>
2022-03-01 13:25:33 -0500

    test rename

--- R/getAndPrepareDataWB_functions.R
+++ R/getAndPrepareDataWB_functions.R
@@ -5,19 +5,35 @@
 #'Extract data from the PIT tag database
 #'
 #'@param drainage Which drainage, "west" or "stanley"#'Extract data from the PIT tag database
+#'@param addCMR add CMR data or not, default is FALSE. This is a placeholder pending decision on how to deal with CMR data
 #'@return a data frame
 #'@export
 
-getCoreData <- function(drainage = "west"){
+getCoreData <- function(drainage = "west", addCMR = FALSE){
 
-  cdWB <- createCoreData(sampleType = "electrofishing", #"stationaryAntenna","portableAntenna"),
-                         whichDrainage = drainage,
-                         columnsToAdd=c("sampleNumber","river","riverMeter","survey",'observedLength','observedWeight')) %>%
-    addTagProperties( columnsToAdd = c("cohort","species","dateEmigrated","sex","species")) %>%
-    dplyr::filter( !is.na(tag), area %in% c("trib","inside","below","above"), !is.na(sampleNumber) ) %>%
-    createCmrData( maxAgeInSamples = 20, inside = F, censorDead = F, censorEmigrated = T) %>%
+  createCoreData(
+      sampleType = "electrofishing",  #"stationaryAntenna","portableAntenna"
+      columnsToAdd = c("sampleNumber",
+                       "river",
+                       "survey",
+                       "observedLength",
+                       "observedWeight"),
+      includeUntagged = TRUE,
+      whichDrainage = drainage
+    ) %>%
+    addTagProperties(
+      columnsToAdd = c("cohort",
+                       "species",
+                       "dateEmigrated",
+                       "sex",
+                       "species"
+      )
+    ) %>%
+    dplyr::filter(species %in% c( "bkt","bnt","ats"),
+                  area %in% c("trib","inside","below","above"),
+                  !is.na(sampleNumber)) %>%
+    #  createCmrData( maxAgeInSamples=20 ) %>%
     addSampleProperties() %>%
-    addEnvironmental() %>%
-    addKnownZ() %>%
-    fillSizeLocation(size = F) #assumes fish stay in same location until observed elsewhere
+    addEnvironmental()
 }
+
